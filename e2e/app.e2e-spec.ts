e3a7e5ae4a9bb65e153ea74bd64bcf70d7e68d54
Bjerkestam Jeanette <JBJPYK@global.scd.scania.com>
2016-11-25 12:34:21 +0100

    Three working e2e tc

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,35 @@
 import { HeroTutorialPage } from './app.po';
 
-describe('hero-tutorial App', function() {
+describe('hero-tutorial App', function () {
   let page: HeroTutorialPage;
 
   beforeEach(() => {
     page = new HeroTutorialPage();
   });
 
-  it('should display message saying app works', () => {
+  it('Verify browser title "HeroTutorial"', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getBrowserTitle()).toEqual('HeroTutorial');
   });
+
+  it('Verify heading "Tour of Heroes" of type h1 in page "Dashboard"', () => {
+    page.navigateTo();
+    page.printHeadingH1InMyApp();
+    expect(page.getHeadingH1InMyApp()).toEqual('Tour of Heroes');
+  });
+
+  it('Verify heading "Top Heroes" of type h3 in page "Dashboard"', () => {
+    page.navigateTo();
+    page.printHeadingH3InMyApp();
+    expect(page.getHeadingH3InMyApp()).toEqual('Top Heroes');
+  });
+
+  // it('Verify change of hero name', () => {
+  //   page.navigateTo();
+  //   page.printOneHeroInList();
+  //   setTimeout(() => { this.router.navigate(['/']); }, 5000);
+  //   page.clickHeroInList();
+  //   page.printHeadingH2InMyHeroDetail();
+  //   expect(page.getHeadingH2InMyHeroDetail()).toEqual('Tornado details!');
+  // });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,46 @@ export class HeroTutorialPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getBrowserTitle() {
+    return browser.getTitle();
   }
+
+  printHeadingH1InMyApp() {
+    console.log('Heading h1 in my-app: ');
+    this.getHeadingH1InMyApp().then(result => console.log(result));
+  }
+  getHeadingH1InMyApp() {
+    return element(by.css('my-app h1')).getText();
+  }
+
+  printHeadingH3InMyApp() {
+    console.log('Heading h3 in my-app: ');
+    this.getHeadingH3InMyApp().then(result => console.log(result));
+  }
+  getHeadingH3InMyApp() {
+    return element(by.css('my-app h3')).getText();
+  }
+
+  printHeadingH2InMyHeroDetail() {
+    console.log('Heading h2 in my-hero-detail: ');
+    this.getHeadingH2InMyHeroDetail().then(result => console.log(result));
+  }
+  getHeadingH2InMyHeroDetail() {
+    return element(by.css('my-hero-detail h2')).getText();
+  }
+  // getListOfHeroes() {
+  //   return [];
+  // }
+
+  printOneHeroInList() {
+    console.log('Print one hero in the list: ');
+    this.getTextForOneHero().then(result => console.log(result));
+  }
+  getTextForOneHero() {
+    return element(by.xpath('//li[10]')).getText();
+  }
+  clickHeroInList() {
+    return element(by.xpath('//li[10]')).click();
+  }
+
 }
