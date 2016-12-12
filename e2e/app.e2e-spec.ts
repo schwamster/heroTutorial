cd047097abd2e64a031ead7ffe3bd93c7da8727f
Bjerkestam Jeanette <JBJPYK@global.scd.scania.com>
2016-12-12 13:37:18 +0100

    Updated

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,53 +9,40 @@ describe('hero-tutorial App', function () {
   });
 
   it('Verify browser title "HeroTutorial"', () => {
-    // page.navigateTo();
     expect(page.getBrowserTitle()).toEqual('HeroTutorial');
   });
 
   it('Verify heading "Tour of Heroes" of type h1 in page "Dashboard"', () => {
-    // page.navigateTo();
-    page.printHeadingH1InMyApp();
     expect(page.getHeadingH1InMyApp()).toEqual('Tour of Heroes');
   });
 
   it('Verify heading "Top Heroes" of type h3 in page "Dashboard"', () => {
-    //page.navigateTo();
-    page.printHeadingH3InMyDashboard();
     expect(page.getHeadingH3InMyDashboard()).toEqual('Top Heroes');
   });
 
   it('Click second hero and verify hero details heading', () => {
-    // page.navigateTo();
     page.getHeroNameForSecondHeroInDashboard().then(heroName => {
-      console.log("Heroname: " + heroName);
       page.clickSecondHeroInDashboard().then(() => {
-        page.printHeadingH2InMyHeroDetail();
         expect(page.getHeadingH2InMyHeroDetail()).toEqual(heroName + ' details!');
       });
     });
   });
 
   it('Naviagate between "Dashboard" and "Heroes"', () => {
-    // page.navigateTo();
-    page.printHeadingH3InMyDashboard();
     expect(page.getHeadingH3InMyDashboard()).toEqual('Top Heroes');
     page.clickButtonHeroes().then(() => {
       page.getHeadingH2InMyHeroes().then(heading => {
-        console.log(`Heading h2 in my-heroes (/heros): ${heading}`);
         expect(heading).toEqual('My Heroes');
       });
     }).then(() => {
       page.clickButtonDashboard().then(() => {
         page.getHeadingH3InMyDashboard().then(heading => {
-          console.log(`Heading h3 in my-app (/dashboard): ${heading}`);
           expect(heading).toEqual('Top Heroes');
         });
       });
     }).then(() => {
       page.clickButtonHeroes().then(() => {
         page.getHeadingH2InMyHeroes().then(heading => {
-          console.log(`Heading h2 in my-heroes (/heros): ${heading}`);
           expect(heading).toEqual('My Heroes');
         });
       });
@@ -63,9 +50,7 @@ describe('hero-tutorial App', function () {
   });
 
   it('Verify change of hero name', () => {
-    // page.navigateTo();
     page.getHeroNameForSecondHeroInDashboard().then(heroName => {
-      console.log("Heroname: " + heroName);
       page.clickSecondHeroInDashboard().then(() => {
         expect(page.getHeadingH2InMyHeroDetail()).toEqual(heroName + ' details!');
       }).then(() => {
@@ -74,10 +59,7 @@ describe('hero-tutorial App', function () {
         }).then(() => {
           page.clickButtonSaveOfHeroDetails().then(() => {
             page.isPresentHeroNameForSecondHeroInDashboard();
-            //expect(page.getHeroNameForSecondHeroInDashboardSlow()).toEqual('Kalle');
             expect(page.getHeroNameForSecondHeroInDashboard()).toEqual('Kalle');
-          }).then(() => {
-            page.printHeroNameForSecondHeroInDashboard();
           });
         });
       });
